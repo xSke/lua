2abb3c1ace14a3c0a2c3d7481f475bff9946dd6f
Ske <voltasalt@gmail.com>
2019-08-25 14:34:26 +0200

    Disable unnecessary modules

--- ltests.c
+++ ltests.c
@@ -877,10 +877,10 @@ static int loadlib (lua_State *L) {
   static const luaL_Reg libs[] = {
     {"_G", luaopen_base},
     {"coroutine", luaopen_coroutine},
-    {"debug", luaopen_debug},
-    {"io", luaopen_io},
-    {"os", luaopen_os},
-    {"math", luaopen_math},
+//    {"debug", luaopen_debug},
+//    {"io", luaopen_io},
+//    {"os", luaopen_os},
+//    {"math", luaopen_math},
     {"string", luaopen_string},
     {"table", luaopen_table},
     {NULL, NULL}
